7f77da6463edddc81f50addeb3edf86a58efc28c
Shobhit <egr.shobhit@gmail.com>
2013-05-20 13:27:55 +0530

    avbf1231

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,16 +21,16 @@ Shobhit1::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  #config.action_mailer.default_url_options = { :host => 'polar-plains-2349.heroku.com' }
-  #config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
+  config.action_mailer.default_url_options = { :host => 'polar-plains-2349.heroku.com' }
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
    address: "smtp.gmail.com",
    port: 587,
    domain: "gmail.com",
-   authentication: "login",
+   authentication: "plain",
    enable_starttls_auto: true,
-   user_name: "shobhit.linkites@gmail.com",
-   password: "06032013"
+   user_name: "testlinkites1@gmail.com",
+   password: "developer1"
   }
 
   # Defaults to nil and saved in location specified by config.assets.prefix
